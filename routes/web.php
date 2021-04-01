53f5204cf81e811a83cd168d2bd2eff093a25a06
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-01 14:09:25 +0100

    Added redirect on root route.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()) {
+        return redirect()->route('home');
+    }
+    return redirect()->route('login');
 });
 
 Auth::routes();
